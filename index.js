69862b2ba9c4786f50050c5317f34cf984bf7ab6
Rajeswaran <rajeswaran149@gmail.com>
2024-05-29 12:25:50 +0530

    update

--- index.js
+++ index.js
@@ -22,8 +22,17 @@ mongoose
 
 const app = express();
 app.use(cors({
-  origin : "*"
+  origin : "*",
+  methods: ["POST", "GET"],
+    credentials: true
 }));
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // '*' allows any origin, you can restrict it to specific origins
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
